8a4af8c13c7b51dde5eaf68492438a4b1caf771e
BrenoNatal <brenonatal3007@gmail.com>
2023-12-14 19:52:42 -0300

    feat: funcao ataque

--- Scripts/Battle/Enemy/enemy.gd
+++ Scripts/Battle/Enemy/enemy.gd
@@ -6,17 +6,22 @@ extends Node2D
 var astar_grid: AStarGrid2D
 var current_path: Array[Vector2i]
 var current_position: Vector2i
+var target: Vector2i
+var damage: int = 3
+var hp: int
 
 func _ready():
 	astar_grid = tile_map.astar_grid
-	
 	astar_grid.set_point_solid(tile_map.local_to_map(global_position))
-
+	hp = 20
+	update_health_bar()
+	
 func move(target):
 	
 	current_position = tile_map.local_to_map(global_position) 
 
 	if current_position in player.get_neighbours(): #ignora o movimento do inimigo se ele ja estiver do lado do jogador
+		attack()
 		return
 	
 	var Path = astar_grid.get_id_path(current_position, target).slice(1)
@@ -24,10 +29,10 @@ func move(target):
 	current_path.append(Path.front())
 	
 	if Path.front(): #atualizar o local anterior o proximo como solido ou não
-		astar_grid.set_point_solid(current_position, false)
 		astar_grid.set_point_solid(Path.front(), true)
+		astar_grid.set_point_solid(current_position, false)
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	if current_path.is_empty():
 		return
 	
@@ -38,3 +43,17 @@ func _physics_process(_delta):
 	if global_position == target:
 		current_path.pop_front()
 		astar_grid.set_point_solid(tile_map.local_to_map(global_position))
+		
+func attack():
+	player.take_damage(damage)
+	
+func take_damage(damage):
+	hp = hp - damage
+	update_health_bar()
+
+func update_health_bar():
+	var health_bar = $HealthBar
+	health_bar.value = hp
+		
+		
+		
--- Scripts/Battle/Player/Player.gd
+++ Scripts/Battle/Player/Player.gd
@@ -1,31 +1,51 @@
 extends Node2D
 
 @onready var tile_map = $"../TileMap"
+@onready var player_health_bar = $"../PlayerHealthBar"
+@onready var enemies = $"../Enemies"
 
 var astar_grid: AStarGrid2D
 var current_path: Array[Vector2i]
 var moving : bool = false
 var range_path : Array[Vector2i]
-var show_range : bool = true
+var show_range : bool = false
 var current_position : Vector2i
+var in_range_enemies = {}
+var hp: int
 
 func _ready():
 	astar_grid = tile_map.astar_grid
 	astar_grid.set_point_solid(tile_map.local_to_map(global_position))
-
+	hp = 100
+	update_health_bar()
 
 func _input(event):
 	if event.is_action_pressed("MouseClick") == false:
 		return
 	
-	if tile_map.local_to_map(get_global_mouse_position()) not in range_path:
+	current_position = tile_map.local_to_map(global_position)
+	var destination = tile_map.local_to_map(get_global_mouse_position())
+	
+	if destination == current_position:
+		show_range = true
+		in_range_enemies = show_enemies_in_range(current_position, 1)
+		print(in_range_enemies)
+		
+	
+	if destination not in range_path and !(in_range_enemies.has(destination)):
 		return
+		
+	if in_range_enemies.has(destination):
+		print("Atacou  ", destination)
+		in_range_enemies.get(destination).take_damage(3)
+		
+		for key in in_range_enemies.keys():
+			tile_map.set_cell(1, Vector2i(key[0],key[1]), -1, Vector2i(-1,-1))
+		in_range_enemies = {}
 	
 	clear_tiles(range_path)
 	range_path.clear()
 	
-	var destination = tile_map.local_to_map(get_global_mouse_position())
-	current_position = tile_map.local_to_map(global_position)
 	
 	var Path = astar_grid.get_id_path(current_position, destination).slice(1)
 	astar_grid.set_point_solid(current_position, false)
@@ -34,17 +54,13 @@ func _input(event):
 	if !Path.is_empty() and !moving:
 		current_path = Path
 		astar_grid.set_point_solid(current_path.back())
-		
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	if show_range and !moving:
 		current_position = tile_map.local_to_map(global_position)
 		range_path = get_tiles_in_range(current_position, 3)
 		show_tiles(range_path)
 		show_range = false
-		
-	if current_path.is_empty() and !moving and range_path.is_empty():
-		show_range = true
 	
 	if current_path.is_empty():
 		current_position = tile_map.local_to_map(global_position)
@@ -58,14 +74,17 @@ func _physics_process(_delta):
 	
 	if global_position == target:
 		current_path.pop_front()
-		
+
 func show_tiles(tiles: Array[Vector2i]):
 	for i in tiles:
 		tile_map.set_cell(1, i, 1, Vector2i(0,0))
-		
+
 func clear_tiles(tiles: Array[Vector2i]):
 	for i in tiles:
 		tile_map.set_cell(1, i, -1, Vector2i(-1,-1))
+	for key in in_range_enemies.keys():
+			tile_map.set_cell(1, Vector2i(key[0],key[1]), -1, Vector2i(-1,-1))
+	in_range_enemies = {}
 
 func get_tiles_in_range(start: Vector2i, range_player: int):
 	var tiles_in_range: Array[Vector2i] = []
@@ -78,14 +97,72 @@ func get_tiles_in_range(start: Vector2i, range_player: int):
 	while  step < range_player:
 		var new_neighbours : Array[Vector2i] = []
 		for i in neighbours:
-			var neighbour_path = astar_grid.get_id_path(start, i)
-			if astar_grid.is_in_boundsv(i) and (i not in tiles_in_range) and (len(neighbour_path) <= range_player + 1 ) and !(astar_grid.is_point_solid(i)):
-				tiles_in_range.append(i)
-				new_neighbours.append_array(tile_map.get_surrounding_cells(i))
+			if astar_grid.is_in_boundsv(i):
+				var neighbour_path = astar_grid.get_id_path(start, i)
+				if (i not in tiles_in_range) and (len(neighbour_path) <= range_player + 1 ) and !(astar_grid.is_point_solid(i)):
+					tiles_in_range.append(i)
+					new_neighbours.append_array(tile_map.get_surrounding_cells(i))
 		neighbours.append_array(new_neighbours)
 		step += 1
 	tiles_in_range.erase(start)
 	return tiles_in_range
-	
+
 func get_neighbours():
 	return tile_map.get_surrounding_cells(current_position)
+
+func take_damage(damage: int):
+	hp = hp - damage
+	update_health_bar()
+
+func update_health_bar():
+	player_health_bar.value = hp
+
+func get_enemies_in_range(start: Vector2i, range: int):
+	
+	var list_enemies : Array[Node]
+	var list_enemies_in_range = {}
+	
+	var enemies_dict = {}
+	
+	list_enemies = enemies.get_children()
+	
+	for enemy in list_enemies:
+		var enemy_position = tile_map.local_to_map(enemy.global_position)
+		enemies_dict[enemy_position] = enemy
+	
+	var tiles_in_range: Array[Vector2i] = []
+	
+	var step = 0
+	
+	var neighbours : Array[Vector2i] = tile_map.get_surrounding_cells(start)
+	neighbours.append(start)
+	
+	while  step < range:
+		var new_neighbours : Array[Vector2i] = []
+		for i in neighbours:
+			if enemies_dict.has(i):
+				list_enemies_in_range[i] = enemies_dict.get(i)
+			if astar_grid.is_in_boundsv(i):
+				var neighbour_path = astar_grid.get_id_path(start, i)
+				if (i not in tiles_in_range) and (len(neighbour_path) <= range + 1 ) and !(astar_grid.is_point_solid(i)):
+					tiles_in_range.append(i)
+					new_neighbours.append_array(tile_map.get_surrounding_cells(i))
+		neighbours.append_array(new_neighbours)
+		step += 1
+		
+	tiles_in_range.erase(start)
+	
+	
+	return list_enemies_in_range
+
+func show_enemies_in_range(start: Vector2i, range: int):
+	var enemies_in_range = get_enemies_in_range(start, range)
+	
+	for key in enemies_in_range.keys():
+		print(key)
+		tile_map.set_cell(1, key, 1, Vector2i(0,0))
+	
+	return enemies_in_range
+
+
+
