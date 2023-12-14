d4a28f8a51515a1f5662d27ca2c268a764f2c18a
Carlos Bravo <carlosh.serrado@gmail.com>
2023-12-14 19:50:52 -0300

    feat: Adiciona enum de cartas

---
+++ Resources/Enums.gd
@@ -0,0 +1,12 @@
+extends Node
+
+class_name Enums
+
+enum CardTypes {
+	ATAQUE,
+	BARREIRA_ROCHOSA,
+	LUFADA,
+	NO_DAGUA,
+	TURBILHAO_CHAMAS,
+	RAIO
+}
--- Scripts/Battle/battle.gd
+++ Scripts/Battle/battle.gd
@@ -4,3 +4,6 @@ signal move_enemies
 
 func _on_turn_manager_pass_turn(_turn):
 	move_enemies.emit()
+
+func _on_deck_system_use_card(card):
+	pass # Replace with function body.
