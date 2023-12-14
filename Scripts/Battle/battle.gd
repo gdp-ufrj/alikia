310fdbc49a850e798c0e3f8a5b98fc413695c9a7
Carlos Bravo <carlosh.serrado@gmail.com>
2023-12-14 20:05:57 -0300

    feat: Adiciona sinal de mover jogador

--- Scripts/Battle/battle.gd
+++ Scripts/Battle/battle.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 signal move_enemies
+signal allow_player_move
 
 func _on_turn_manager_pass_turn(_turn):
 	move_enemies.emit()
+	allow_player_move.emit()
 
 func _on_deck_system_use_card(card):
 	pass # Replace with function body.
