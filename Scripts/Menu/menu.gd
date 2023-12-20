b691d7e7ba6679c3b98129bdfdbd5e9617d1554d
Carlos Bravo <carlosh.serrado@gmail.com>
2023-12-20 15:57:31 -0300

    qol: Muda funções para inglês

--- Scripts/Menu/menu.gd
+++ Scripts/Menu/menu.gd
@@ -3,18 +3,16 @@ extends Control
 @onready var ui_accept = $uiAccept
 @onready var ui_decline = $uiDecline
 
-func _on_começar_pressed():
+func _on_start_pressed():
 	ui_accept.play()
 	get_tree().change_scene_to_file("res://Scenes/cutscene.tscn")
 
-
-func _on_creditos_pressed():
+func _on_credits_pressed():
 	ui_accept.play()
 	#get_tree().change_scene_to_file("res://Scenes/credits.tscn")
 	print("Créditos")
 
-func _on_sair_pressed():
+func _on_exit_pressed():
 	print("Sair")
 	ui_decline.play()
 	get_tree().quit()
-	
